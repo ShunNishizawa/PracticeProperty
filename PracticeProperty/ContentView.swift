d47149212632af56c2eb0b144879092088d5ccdf
NishizawaShun <nishizawa.shun@appsoftlab.com>
2022-04-29 11:38:13 +0900

    ObservedObjectについて学習

---
+++ PracticeProperty/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  PracticeProperty
+//
+//  Created by 西澤駿 on 2022/04/29.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        ObservedScreen()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
